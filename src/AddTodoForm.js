e2729727c79cb6eebcf8cec2dcf79c5c21004043
Raheim Grant <rgran065@fiu.edu>
2023-04-17 00:45:11 -0400

    lesson1_3

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
-export default function AddTodoForm() {
+export default function AddTodoForm(props) {
+  
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const todoTitle = event.target.elements.title.value;
+    props.onAddTodo(todoTitle);
+    event.target.reset();
+  }
+  
   return (
-    <form> 
-        <label htmlFor="todoTitle">Title</label>
-        <input id= "todoTitle"></input>
+    <form onSubmit={handleAddTodo}> 
+        <label htmlFor="todoTitle">Title:</label>
+        <input id= "todoTitle" name="title"></input>
         <button type= "submit">Add</button>
     </form>
-  )
+  );
 }
