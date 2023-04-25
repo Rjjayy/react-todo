035dccc33bac6509edf89dd98dd34d30d8fcd768
Raheim Grant <rgran065@fiu.edu>
2023-04-25 00:30:02 -0400

    lesson1_5

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+}
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
+
   
  function addTodo (newTodo) {
     setTodoList(prevTodoList => [...prevTodoList, newTodo]);
  }
   return (
 
-    <div>
+    <>
       <h1>Todo List</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
   
