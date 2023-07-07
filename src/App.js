f485f6479c5d23c0ac8f8fd098efd6b3b312b7b2
Raheim Grant <rgran065@fiu.edu>
2023-07-06 22:02:24 -0400

    Sort by Airtable view order(5.1)

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   async function fetchData() {
-    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?view=Grid%20view`;
+    const url = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE_ID}/${process.env.REACT_APP_TABLE_NAME}?view=Grid%20view&sort[0][field]=title&sort[0][direction]=asc`;
     const options = {
       method: "GET",
       headers: {
